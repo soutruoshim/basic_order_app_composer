b89dfabccd55d0026bb882971f96bdb4861c4804
sout.rahim <>
2023-04-11 14:11:48 +0700

    viewmodel

---
+++ app/src/main/java/com/example/composerdemo1/ui/OrderViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.composerdemo1.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class OrderViewModel: ViewModel() {
+
+    // Ui state
+    var name by mutableStateOf("")
+    private set
+    var phone by mutableStateOf("")
+    private set
+
+
+    //Ui event
+    fun onNameChange():(String)->Unit = {
+        name = it
+    }
+
+    fun onPhoneChange():(String)->Unit = {
+        name = it
+    }
+
+    fun payOrder():()->Unit = {
+       println("Order by $name, and phone $phone")
+    }
+
+}
